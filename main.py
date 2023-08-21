e451df901997c4d7d6c81ba941c473db4bad8bcd
Ali Albayrak <l1120910@std.yildiz.edu.tr>
2023-08-21 17:10:24 +0300

    implement get_numbers function
    implement function that read the input from user and validate it

--- main.py
+++ main.py
@@ -15,10 +15,21 @@ def get_numbers()->list[int]:
     """
     Read a list of positive integers from the user, validate the inputs, and return the list.
     """
-    pass
+    while True:
+        try:
+            input_str = input("Enter a list of positive integers separated by spaces: ")
+            input_numbers = [int(x) for x in input_str.split()]
+            
+            if any(num <= 0 for num in input_numbers):
+                raise ValueError("All numbers must be positive integers")
+            
+            return input_numbers
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please try again.")
 
 def main():
-    print("Initialize")
+    numbers = get_numbers()
+    print("your input list: ",numbers)
 
 if __name__ == "__main__":
     main()
