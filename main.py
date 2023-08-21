4756ad2b8ed2c9727dc84fd9ea9740520bfc1607
Ali Albayrak <l1120910@std.yildiz.edu.tr>
2023-08-21 16:01:27 +0300

    Creat main.py
    Initialize functions, and file structure

---
+++ main.py
@@ -0,0 +1,24 @@
+def gcd_two_numbers(a: int, b: int)->int:
+    """
+    Calculate GCD of two positive integers using the Euclidean Algorithm.
+    """
+    pass
+
+def gcd_numbers(numbers: list[int])->int:
+    """
+    Calculate GCD of a list of positive integers.
+    Uses the gcd_two_numbers function.
+    """
+    pass
+
+def get_numbers()->list[int]:
+    """
+    Read a list of positive integers from the user, validate the inputs, and return the list.
+    """
+    pass
+
+def main():
+    print("Initialize")
+
+if __name__ == "__main__":
+    main()
